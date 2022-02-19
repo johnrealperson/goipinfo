30d16e94fdc6bcce96a15715f05b3dc2481d3613
johnisarealperson <johnisarealperson2@gmail.com>
2022-02-19 12:04:09 -0600

    Pushing changes to ipinfo

--- ipinfo.go
+++ ipinfo.go
@@ -4,11 +4,21 @@ import (
 	"bufio"
 	"fmt"
 	"io/ioutil"
+
+	//"log"
 	"net/http"
 	"os"
+	//"github.com/joho/godotenv"
 )
 
 func main() {
+	//enverr := godotenv.Load()
+	//if enverr != nil {
+	//	log.Fatal("Error loading .env file")
+	//}
+
+	//apiKey := os.Getenv("API_KEY")
+	apiKey := "c241bec690d0f217240381d069b6d41ee61bbf2f88f0f4829a15d4a2e3b4bfdd"
 	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Print("IP: ")
 	scanner.Scan()
@@ -18,13 +28,20 @@ func main() {
 	req, _ := http.NewRequest("GET", url, nil)
 
 	req.Header.Add("Accept", "application/json")
-	req.Header.Add("x-apikey", "c241bec690d0f217240381d069b6d41ee61bbf2f88f0f4829a15d4a2e3b4bfdd")
+	req.Header.Add("x-apikey", apiKey)
 
-	res, _ := http.DefaultClient.Do(req)
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
 
 	defer res.Body.Close()
-	body, _ := ioutil.ReadAll(res.Body)
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
 
 	fmt.Println(res)
 	fmt.Println(string(body))
+
 }
