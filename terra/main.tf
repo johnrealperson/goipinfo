42658eaa9d00ae3fe6a802743db536476de46138
johnrealperson <99989160+johnrealperson@users.noreply.github.com>
2022-02-21 05:25:39 -0600

    Update main.tf

--- terra/main.tf
+++ terra/main.tf
@@ -21,7 +21,7 @@ name = "terraform-network"
     template { 
         spec {
             containers {
-                image = "us.gcr.io/flash-flow-341718/gcf/us-central1/f33e13bb-fdd4-4c9f-8295-ba6a1c778d9d@sha256:64bc2f33b7d2e795114b9a8c0bd7c10b875b921432d3e4961e95b9cd0ec5e718"
+                image = "YOUR_DOCKER_CONTAINER_HERE"
             }
         }    
     }
